805dd4bb9d7d3907e3f9a93110cbd77fbd7bd6b4
Aleksandr Shakhov <A.SHahov@ase-ec.ru>
2024-09-22 22:22:12 +0300

    added patients

---
+++ patients/src/main/java/ru/lims/patients/PatientsApplication.java
@@ -0,0 +1,13 @@
+package ru.lims.patients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientsApplication.class, args);
+    }
+
+}
---
+++ patients/src/test/java/ru/lims/patients/PatientsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lims.patients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
