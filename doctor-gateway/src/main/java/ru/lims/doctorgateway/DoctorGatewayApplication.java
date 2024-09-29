0c1d47db1c9443ff5b7ab5ede52c522401cd1123
Aleksandr Shakhov <A.SHahov@ase-ec.ru>
2024-09-29 13:54:58 +0300

    added doctor-gateway module

---
+++ doctor-gateway/src/main/java/ru/lims/doctorgateway/DoctorGatewayApplication.java
@@ -0,0 +1,13 @@
+package ru.lims.doctorgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoctorGatewayApplication.class, args);
+    }
+
+}
