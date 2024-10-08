ba68d5f7b1dcda2ad10f1c818f50887465765fbe
Aleksandr Shakhov <A.SHahov@ase-ec.ru>
2024-10-08 21:26:29 +0300

    added services (doctor-gateway)

---
+++ doctor-gateway/src/main/java/ru/lims/doctorgateway/service/AnalysisService.java
@@ -0,0 +1,20 @@
+package ru.lims.doctorgateway.service;
+
+import java.net.URI;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Service
+public class AnalysisService {
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+
+    public Long create(Long testId) {
+        URI uri = UriComponentsBuilder.fromHttpUrl("http://localhost:8083/analysis")
+            .queryParam("testId", testId)
+            .build().encode().toUri();
+        return restTemplate.postForObject(uri, null, Long.class);
+    }
+}
---
+++ doctor-gateway/src/main/java/ru/lims/doctorgateway/service/ConsultationService.java
@@ -0,0 +1,29 @@
+package ru.lims.doctorgateway.service;
+
+import java.net.URI;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+import ru.lims.doctorgateway.dto.ConsultationUpdateDto;
+
+@Service
+public class ConsultationService {
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @Autowired
+    private ConsultationGateway consultationGateway;
+
+    public Long create(Long medicalHistoryId) {
+        URI uri = UriComponentsBuilder.fromHttpUrl("http://localhost:8081/consultation")
+            .queryParam("medicalHistoryId", medicalHistoryId)
+            .build().encode().toUri();
+        return restTemplate.postForObject(uri, null, Long.class);
+    }
+
+    public long update(ConsultationUpdateDto consultationUpdateDto) {
+        consultationGateway.processConsultation(consultationUpdateDto);
+        return 0;
+    }
+}
