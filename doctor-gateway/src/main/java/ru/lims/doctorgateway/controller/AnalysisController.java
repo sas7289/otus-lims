9a749aa839e5765dec766a7af60eea58d048647a
Aleksandr Shakhov <A.SHahov@ase-ec.ru>
2024-10-13 23:43:31 +0300

    added swagger

--- doctor-gateway/src/main/java/ru/lims/doctorgateway/controller/AnalysisController.java
+++ doctor-gateway/src/main/java/ru/lims/doctorgateway/controller/AnalysisController.java
@@ -1,18 +1,24 @@
 package ru.lims.doctorgateway.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import ru.lims.doctorgateway.service.AnalysisService;
 
-@Controller
+@RestController
 public class AnalysisController {
 
     @Autowired
     private AnalysisService analysisService;
 
+
+    @Operation(summary = "Создать новый анализ для теста с ID testId", description = "Создаёт на микросервисе, отвечающем за исследования заготовку"
+        + "для нового анализа")
+    @ApiResponse(responseCode = "200", description = "Анализ успешно создан")
     @PostMapping("/analysis")
     public ResponseEntity<Long> create(@RequestParam("testId") Long testId) {
         Long analysisId = analysisService.create(testId);
--- doctor-gateway/src/main/java/ru/lims/doctorgateway/controller/ConsultationController.java
+++ doctor-gateway/src/main/java/ru/lims/doctorgateway/controller/ConsultationController.java
@@ -1,31 +1,41 @@
 package ru.lims.doctorgateway.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import ru.lims.doctorgateway.dto.ConsultationUpdateDto;
 import ru.lims.doctorgateway.service.ConsultationService;
 
-@Controller
+@RestController
 public class ConsultationController {
 
     @Autowired
     private ConsultationService consultationService;
 
+    @Operation(summary = "Создать новый анализ для медицинской истории с ID medicalHistoryId",
+        description = "Создаёт заготовку для новой консультации")
+    @ApiResponse(responseCode = "200", description = "Консультация успешно создана")
     @PostMapping("/consultation")
     public ResponseEntity<Long> create(@RequestParam(name = "medicalHistoryId") Long medicalHistoryId) {
         long consultationId = consultationService.create(medicalHistoryId);
         return ResponseEntity.ok(consultationId);
     }
 
+    @Operation(summary = "Обновить информацию о консультации и назначенных исследованиях",
+        description = "Обновляет информацию о консультации и назначенных исследованиях")
+    @ApiResponse(responseCode = "200", description = "Консультация успешно обнолвена")
     @PutMapping("/consultation")
-    public ResponseEntity<Long> update(@RequestBody ConsultationUpdateDto consultationUpdateDto) {
-        long consultationId = consultationService.update(consultationUpdateDto);
-        return ResponseEntity.ok(consultationId);
+    @Valid
+    public ResponseEntity<Long> update(@Valid @RequestBody ConsultationUpdateDto consultationUpdateDto) {
+        consultationService.update(consultationUpdateDto);
+        return ResponseEntity.ok().build();
     }
 
 }
