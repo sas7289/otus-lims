b245273c8346da9591596b87d2b622a232a166bc
Aleksandr Shakhov <A.SHahov@ase-ec.ru>
2024-10-13 23:51:43 +0300

    added swagger

--- employees/src/main/java/ru/lims/employees/controller/EmployeeController.java
+++ employees/src/main/java/ru/lims/employees/controller/EmployeeController.java
@@ -1,19 +1,28 @@
 package ru.lims.employees.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 import ru.lims.employees.dto.EmployeeDto;
 import ru.lims.employees.service.EmployeeService;
 
-@Controller
+@RestController
 public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
 
+
+    @Operation(summary = "Получить информацию о сотруднике",
+        description = "Возвращает информацю о сотруднике по его ID")
+    @ApiResponse(responseCode = "200",
+        content = @Content(mediaType = "application/json", schema = @Schema(implementation = EmployeeDto.class)))
     @GetMapping("/employee/{id}")
     public ResponseEntity<EmployeeDto> getEmployee(@PathVariable long id) {
         EmployeeDto employee = employeeService.getEmployee(id);
