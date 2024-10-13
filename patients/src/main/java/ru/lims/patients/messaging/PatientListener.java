c42efa40beb5effde8720f17a9528c32bb854f76
Aleksandr Shakhov <A.SHahov@ase-ec.ru>
2024-10-13 23:53:37 +0300

    improved messaging

--- patients/src/main/java/ru/lims/patients/messaging/PatientListener.java
+++ patients/src/main/java/ru/lims/patients/messaging/PatientListener.java
@@ -1,14 +1,26 @@
 package ru.lims.patients.messaging;
 
+import lombok.AllArgsConstructor;
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.lims.patients.dto.ConsultationUpdateDto;
+import ru.lims.patients.service.ConsultationService;
 
 @Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PatientListener {
 
-    @RabbitListener(queues = "${rabbitmq.queue.doctor-gateway-to-patient}")
-    public void consume(String message) {
-        System.out.println("Rabbit message: " + message);
-    }
+    private ConsultationService consultationService;
 
+    @RabbitListener(queues = "${rabbitmq.queue.doctor-gateway-to-patient}")
+    public void consume(ConsultationUpdateDto consultationUpdateDto) {
+        try {
+            consultationService.update(consultationUpdateDto);
+            System.out.println("Rabbit message: " + consultationUpdateDto);
+        } catch (Exception e) {
+            throw new AmqpRejectAndDontRequeueException(e);
+        }
+    }
 }
