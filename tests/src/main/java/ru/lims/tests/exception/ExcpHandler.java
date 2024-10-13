4008cc217ca111a7642e63e4d874165da1192b5c
Aleksandr Shakhov <A.SHahov@ase-ec.ru>
2024-10-13 23:55:32 +0300

    improvements

---
+++ tests/src/main/java/ru/lims/tests/exception/ExcpHandler.java
@@ -0,0 +1,25 @@
+package ru.lims.tests.exception;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+@Slf4j
+public class ExcpHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> globalExceptionHandler(Exception ex) {
+        return ResponseEntity.badRequest().body(getStacktrace(ex));
+
+    }
+
+    public String getStacktrace(Exception exception) {
+        StringWriter stringWriter = new StringWriter();
+        exception.printStackTrace(new PrintWriter(stringWriter));
+        log.error(stringWriter.toString());
+        return exception.getMessage();
+    }
+}
--- tests/src/main/java/ru/lims/tests/service/AnalysisService.java
+++ tests/src/main/java/ru/lims/tests/service/AnalysisService.java
@@ -4,8 +4,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.lims.tests.dto.AnalysisDto;
+import ru.lims.tests.exception.ObjectNotFoundException;
 import ru.lims.tests.mapper.AnalysisMapper;
+import ru.lims.tests.model.Analysis;
+import ru.lims.tests.model.Test;
 import ru.lims.tests.repository.AnalysisRepository;
+import ru.lims.tests.repository.TestRepository;
 
 @Service
 public class AnalysisService {
@@ -16,10 +20,31 @@ public class AnalysisService {
     @Autowired
     private AnalysisRepository analysisRepository;
 
+    @Autowired
+    private TestRepository testRepository;
+
     public List<AnalysisDto> getAnalysis(long patientId) {
         return analysisRepository.findAllByPatientId(patientId).stream()
             .map(analysisMapper::toDto)
             .toList();
     }
 
+    public Long create(Long testId) {
+        Test test = testRepository.findById(testId)
+            .orElseThrow(() -> new ObjectNotFoundException("Test not found by ID: " + testId));
+        Analysis analysis = new Analysis();
+        analysis.setTest(test);
+        Analysis savedAnalysis = analysisRepository.save(analysis);
+        return savedAnalysis.getId();
+    }
+
+    public void update(List<AnalysisDto> analyzes) {
+        analyzes.stream()
+            .map(analysisMapper::fromDto)
+            .forEach(this::update);
+    }
+
+    private void update(Analysis analysis) {
+        analysisRepository.save(analysis);
+    }
 }
