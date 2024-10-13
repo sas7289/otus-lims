5b8b6a487e5106b101e499a4a8da10f9f0553b41
Aleksandr Shakhov <A.SHahov@ase-ec.ru>
2024-10-13 23:54:20 +0300

    added swagger and bean validation

--- tests/src/main/java/ru/lims/tests/controller/AnalysisController.java
+++ tests/src/main/java/ru/lims/tests/controller/AnalysisController.java
@@ -1,5 +1,9 @@
 package ru.lims.tests.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,18 +23,31 @@ public class AnalysisController {
     @Autowired
     private AnalysisService analysisService;
 
+    @Operation(summary = "Получить информацию об анализацх пациента по его ID",
+        description = "Возвращает информацю об анализацх пациента по его ID")
+    @ApiResponse(responseCode = "200",
+        content = @Content(mediaType = "application/json", schema = @Schema(implementation = AnalysisDto.class)))
     @GetMapping("/analysis/patient/{patientId}")
     public ResponseEntity<List<AnalysisDto>> getAnalysisByPatientId(@PathVariable long patientId) {
         List<AnalysisDto> analysis = analysisService.getAnalysis(patientId);
         return ResponseEntity.ok(analysis);
     }
 
+    @Operation(summary = "Создать новый анализ",
+        description = "Возвращает ID нового анализа")
+    @ApiResponse(responseCode = "200",
+        description = "Анализ успешно создан",
+        content = @Content(mediaType = "application/json", schema = @Schema(implementation = Long.class)))
     @PostMapping("/analysis")
     public ResponseEntity<Long> create(@RequestParam("testId") Long testId) {
         Long analysisId = analysisService.create(testId);
         return ResponseEntity.ok(analysisId);
     }
 
+    @Operation(summary = "Обновить список анализов",
+        description = "Обновляет список анализов")
+    @ApiResponse(responseCode = "200",
+        description = "Анализы успешно обновлены")
     @PutMapping("/analysis")
     public ResponseEntity<Void> update(@RequestBody List<AnalysisDto> analyzes) {
         analysisService.update(analyzes);
--- tests/src/main/java/ru/lims/tests/dto/AnalysisDto.java
+++ tests/src/main/java/ru/lims/tests/dto/AnalysisDto.java
@@ -1,5 +1,8 @@
 package ru.lims.tests.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,19 +13,27 @@ import lombok.NoArgsConstructor;
 @Data
 public class AnalysisDto {
 
+    @NotNull
+    @Positive
     private long id;
 
+    @NotNull
     private LocalDateTime appointmentDate;
 
     private TestDto test;
 
     private BiomaterialDto biomaterial;
 
+    @NotBlank
     private String status;
 
+    @NotNull
+    @Positive
     private long performedById;
 
     private TestResultDto testResult;
 
+    @NotNull
+    @Positive
     private long patientId;
 }
